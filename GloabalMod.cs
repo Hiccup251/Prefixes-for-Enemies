8bf7faa98ecd72222ee8f72f160dc8ef877181bd
Hiccup251 <kslagell@gmail.com>
2016-03-28 19:36:29 -0500

    Added files via upload

---
+++ GloabalMod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EnemyMods
+{
+    public class GloabalMod : Mod
+    {
+        public override void SetModInfo(out string name, ref ModProperties properties)
+        {
+            name = "EnemyMods";
+            properties.Autoload = true;
+        }
+    }
+}
