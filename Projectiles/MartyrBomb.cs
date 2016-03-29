d2fd17daba2ccbcfd3c068834f25c2a69768d8c4
Hiccup251 <kslagell@gmail.com>
2016-03-28 19:43:24 -0500

    Added files via upload

---
+++ Projectiles/MartyrBomb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+
+namespace EnemyMods.Projectiles
+{
+    public class MartyrBomb : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.HappyBomb);
+            projectile.timeLeft = 120;
+            projectile.hostile = false;
+        }
+        public override void Kill(int timeLeft)
+        {
+            int p = Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0, 0, ProjectileID.Landmine, projectile.damage, 0);
+        }
+    }
+}
